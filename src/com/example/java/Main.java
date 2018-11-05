44e818bfb22541839767c6608e06d99a15e5d0eb
Aaron VanAlstine <aaronvan@icloud.com>
2018-11-05 08:57:51 -0800

    Refactor Main

--- src/com/example/java/Main.java
+++ src/com/example/java/Main.java
@@ -13,41 +13,42 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner console = new Scanner(System.in);
         String dataTarget = "files/data.json";
-
         System.out.print("Enter the movie title: ");
         String title = console.nextLine();
 
         // convert the movie title to a search string URL
         String dataSource = Movie.getAPISearchString(title);
-        // create a URL connection to the OMDb using the search string
-        URL connect = new URL(dataSource);
+        // create a URL to the OMDb using the search string
+        URL url = new URL(dataSource);
         // create a URLConnection object to get the content from the OMBd
         // and open a connection
-        URLConnection OMDbCon = connect.openConnection();
+        URLConnection urlConnection = url.openConnection();
 
         // create a buffered reader object to get an input stream from the connection and
         // write it to the the file for eventual parsing
         try (
-             BufferedReader in = new BufferedReader(new InputStreamReader(OMDbCon.getInputStream()));
-             FileWriter fw = new FileWriter(dataTarget)
-                ) {
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+             FileWriter fileWriter = new FileWriter(dataTarget)
+            )
+        {
             while (true) {
-                String line = in.readLine();
+                String line = bufferedReader.readLine();
                 if (line == null) {
                     break;
                 }
-                fw.write(line + "\n");
+                fileWriter.write(line + "\n");
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         Gson gson = new Gson();
-        try (FileReader fileReader = new FileReader("files/data.json");
-            JsonReader reader = new JsonReader(fileReader)
+        try (
+            FileReader fileReader = new FileReader("files/data.json");
+            JsonReader jsonReader = new JsonReader(fileReader)
             )
         {
-            Movie movie = gson.fromJson(reader, Movie.class);
+            Movie movie = gson.fromJson(jsonReader, Movie.class);
             System.out.println("Movie: " + movie.getTitle());
             System.out.println("Year: " + movie.getYear());
             System.out.println("Genre: " + movie.getGenre());
@@ -55,8 +56,6 @@ public class Main {
             System.out.println("Actors: " + movie.getActors());
             System.out.println("Plot: " + movie.getPlot());
         }
-
     }
-
 
 }
